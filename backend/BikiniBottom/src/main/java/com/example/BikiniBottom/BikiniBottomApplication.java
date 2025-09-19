1386f305d14ea972a041877e300a0378ed5344a3
Quinn-qhy <quinn.qhyang@gmail.com>
2025-09-19 20:21:28 +0100

    初始化项目

---
+++ backend/BikiniBottom/src/main/java/com/example/BikiniBottom/BikiniBottomApplication.java
@@ -0,0 +1,13 @@
+package com.example.BikiniBottom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikiniBottomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikiniBottomApplication.class, args);
+	}
+
+}
---
+++ backend/BikiniBottom/src/test/java/com/example/BikiniBottom/BikiniBottomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BikiniBottom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikiniBottomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
